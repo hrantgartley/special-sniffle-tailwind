0ba20255c3c2e6682b60e4ec255d6630cd88aa73
hrantgartley <doyouevenjump@gmail.com>
2024-02-17 20:14:43 -0600

    phpdoc

--- index.php
+++ index.php
@@ -8,37 +8,44 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-    <link rel="stylesheet" href="dist/styles.css">
+    <link rel="stylesheet" href="src/styles.css">
 
 </head>
 
 <body>
     <main>
-        <form method="post">
-            <?php
-            function echoSelected($value, $selected) {
-                if ($value == $selected) {
-                    echo 'selected';
-                }
+        <?php
+
+
+        /**
+         * Prints the "selected" attribute if the given value matches the selected value.
+         *
+         * @param mixed $value The value to compare.
+         * @param mixed $selected The selected value.
+         * @return void
+         */
+        function printSelectedValueFromSelect($value, $selected) {
+            if ($value == $selected) {
+                echo 'selected';
             }
-            $selectedValue = isset($_POST['cars']) ? $_POST['cars'] : '';
-            ?>
-            <select name="cars" id="cars" required class="bg-red-500">
+        }
+        $selectedValue = isset($_POST['cars']) ? $_POST['cars'] : '';
+
+        ?>
+        <form method="post">
+            <select name="cars" id="cars" required>
                 <option value="">None</option>
-                <option value="volvo" <?php echoSelected('volvo', $selectedValue); ?>>Volvo</option>
-                <option value="camry" <?php echoSelected('camry', $selectedValue); ?>>Camry</option>
-                <option value="audi" <?php echoSelected('audi', $selectedValue); ?>>Audi</option>
+                <option value="volvo" <?php printSelectedValueFromSelect('volvo', $selectedValue); ?>>Volvo</option>
+                <option value="camry" <?php printSelectedValueFromSelect('camry', $selectedValue); ?>>Camry</option>
+                <option value="audi" <?php printSelectedValueFromSelect('audi', $selectedValue); ?>>Audi</option>
             </select>
             <br>
             <button type="submit" class="btn btn-primary text-red-600">Submit</button>
         </form>
         <?php
+        $filterd_data = filter_input(INPUT_POST, 'cars', FILTER_VALIDATE_REGEXP, array("options" => array("regexp" => "/^(volvo|camry|audi)$/")));
         echo "Value that user selected was " . $selectedValue;
         ?>
-        <form action="./includes/process.php" method="post">
-            <input type="text" name="name" id="name" required>
-            <button type="submit" class="btn btn-primary text-orange-600">Submit</button>
-        </form>
     </main>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
 
